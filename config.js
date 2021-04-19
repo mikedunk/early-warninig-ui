11cf32ee502e4b8833e9eba3b47f8cb6b2a88e16
Muyiwa Ayeyemi <mikedunk01@gmail.com>
2021-04-19 15:29:33 +0100

    prod config

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 
 module.exports = {
     base_url: process.env.VUE_APP_BASE_URL,
+    api_url:process.env.API_SERVER,
     configureWebpack: {
         devtool: 'source-map'
       }
